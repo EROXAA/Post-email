d44630c33ad6a413a9de53cb12df9614919a57da
Ernar <114827366+EROXAA@users.noreply.github.com>
2022-10-21 21:15:33 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -33,12 +33,12 @@ try {
 
   // Настройки вашей почты
   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
+  $mail->Username   = 'ernar.fullstack@gmail.com'; // Логин на почте
+  $mail->Password   = 'mrggcigfsfvubfkc'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
-  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
+  $mail->setFrom('ernar.fullstack@gmail.com', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
   $mail->addAddress('');
